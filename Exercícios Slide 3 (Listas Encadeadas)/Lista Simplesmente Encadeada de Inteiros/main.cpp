b43cbef4db9a441de913b28f7fa2a3d293827fc4
Lucas Guimaraes <lucaslgr1206@gmail.com>
2019-05-11 14:46:57 -0300

    Lista simplesmente encadeada de inteiros corrigida

--- Exercícios Slide 3 (Listas Encadeadas)/Lista Simplesmente Encadeada de Inteiros/main.cpp
+++ Exercícios Slide 3 (Listas Encadeadas)/Lista Simplesmente Encadeada de Inteiros/main.cpp
@@ -5,31 +5,31 @@ using namespace std;
 
 int main()
 {
-    Lista l1;
+    Lista *l1 = new Lista();
 
-    l1.insereInicio(20);
-    l1.insereInicio(30);
+    l1->insereInicio(20);
+    l1->insereInicio(30);
 
-    l1.insereFinal(40);
-    l1.insereFinal(60);
+    l1->insereFinal(40);
+    l1->insereFinal(60);
 
-    l1.imprime();
+    l1->imprime();
     
-    l1.removeInicio(); //Ta com erro
+    l1->removeInicio(); //Ta com erro
 
-    l1.imprime();
+    l1->imprime();
 
-    l1.removeFinal();
+    l1->removeFinal();
 
-    l1.imprime();
+    l1->imprime();
 
-    l1.removePorValor(40);
+    l1->removePorValor(40);
     
-    l1.imprime();
+    l1->imprime();
 
-    l1.removePorValor(20);
+    l1->removePorValor(20);
 
-    l1.imprime();
+    l1->imprime();
 
     return 0;
 }
